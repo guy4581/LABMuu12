25ddcc1e4e95f013e0c59184832b0d53296090d8
Your Name <you@example.com>
2023-12-26 15:12:45 +0700

    completed

--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+template<typename T>
+void mySwap(T &x,T &y);
+
+
+
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +33,11 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+template<typename T>
+void mySwap(T &x,T &y){
+	T a = x;
+	x=y;
+	y=a;
 }
 
